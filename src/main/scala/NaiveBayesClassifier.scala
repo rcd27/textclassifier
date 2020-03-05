86956e9282c5ba2a02f6ac7fa809fc6da8b82b97
Stanislav Zemlyakov <redtom@yandex.ru>
2020-03-05 14:39:29 +0300

    Cleanup

--- src/main/scala/NaiveBayesClassifier.scala
+++ src/main/scala/NaiveBayesClassifier.scala
@@ -12,11 +12,7 @@ class NaiveBayesClassifier(model: NaiveBayesModel) {
         .maxBy(_._2)(Ordering.Double.TotalOrdering)
         ._1
 
-    val wordsForCurrentClass: Seq[(Term, Double)] = wordProbability(docClass, tokenizedText)
-    val top3words = wordsForCurrentClass
-      .sortBy(pair => pair._2)
-      .take(3)
-
+    val top3words: Seq[Term] = calculateTop3words(docClass, tokenizedText)
     val arr: ArrayBuffer[Char] = ArrayBuffer.empty
     inputRawText.toIndexedSeq
       .zipWithIndex
@@ -24,17 +20,17 @@ class NaiveBayesClassifier(model: NaiveBayesModel) {
         val currentChar = cd._1
         val currentId = cd._2
         // search for current index in top3word
-        val currentCharIsStart: Option[(Term, Double)] = top3words.find(td => td._1.start == currentId)
-        val currentCharIsEnd: Option[(Term, Double)] = top3words.find(td => td._1.end == currentId + 1)
+        val currentCharIsStart: Option[Term] = top3words.find(_.start == currentId)
+        val currentCharIsEnd: Option[Term] = top3words.find(_.end == currentId + 1)
 
-        // TODO: переделать на pattern matching
+        // TODO: можно ли переделать на pattern matching?
         if (currentCharIsStart.nonEmpty) {
           arr.addOne('*')
           arr.addOne(currentChar)
         } else if (currentCharIsEnd.nonEmpty) {
           arr.addOne(currentChar)
           arr.addOne('*')
-        } else {
+        } else { // чтобы это был дефолтный кейс "_"
           arr.addOne(currentChar)
         }
       })
@@ -52,8 +48,11 @@ class NaiveBayesClassifier(model: NaiveBayesModel) {
     wordProbability + model.classLogProbability(`class`)
   }
 
-  private def wordProbability(`class`: DocClass, tokenizedText: Vector[Term]): Vector[(Term, Double)] = {
+  private def calculateTop3words(`class`: DocClass, tokenizedText: Vector[Term]): Vector[Term] = {
     tokenizedText
       .map(term => (term, model.wordLogProbability(`class`, term.word)))
+      .sortBy(pair => pair._2)
+      .take(3)
+      .map(pair => pair._1)
   }
 }

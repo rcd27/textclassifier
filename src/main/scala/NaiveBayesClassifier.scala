bbb1e459a413244f39c7b69b989a4b73249084b0
Stanislav Zemlyakov <redtom@yandex.ru>
2020-03-05 12:47:38 +0300

    Зелёный тест для подсвечивалки слов

--- src/main/scala/NaiveBayesClassifier.scala
+++ src/main/scala/NaiveBayesClassifier.scala
@@ -1,21 +1,33 @@
 class NaiveBayesClassifier(model: NaiveBayesModel) {
 
   def classify(text: String): DocClassification = {
+    // FIXME: два раза tokenize, при чём из разных классов
+    val tokenizedText: Vector[Term] = PorterAnalyzer.tokenize(text)
+
     val docClass: DocClass =
       model.classes
-        .map(c => (c, calculateProbability(c, text)))
+        .map(c => (c, calculateProbability(c, tokenizedText)))
         .maxBy(_._2)(Ordering.Double.TotalOrdering)
         ._1
-    val highlightedText: HighlightedText = new HighlightedText("some *words* highlighted")
-    new DocClassification(docClass, highlightedText)
+
+    val wordsForCurrentClass: Seq[(Term, Double)] = wordProbability(docClass, tokenizedText)
+    val top3words = wordsForCurrentClass
+      .sortBy(pair => pair._2)
+      .take(3)
+
+    new DocClassification(docClass, new HighlightedText("*highlightedText*"))
   }
 
   /* Count a probability of document for a class */
-  private def calculateProbability(`class`: DocClass, text: String): Double = {
-    // FIXME: два раза tokenize, при чём из разных классов
-    val wordProbability = PorterAnalyzer.tokenize(text)
+  private def calculateProbability(`class`: DocClass, tokenizedText: Vector[Term]): Double = {
+    val wordProbability = tokenizedText
       .map(term => model.wordLogProbability(`class`, term.word))
       .sum
     wordProbability + model.classLogProbability(`class`)
+  }
+
+  private def wordProbability(`class`: DocClass, tokenizedText: Vector[Term]): Vector[(Term, Double)] = {
+    tokenizedText
+      .map(term => (term, model.wordLogProbability(`class`, term.word)))
   }
 }
--- src/test/scala/NaiveBayesClassifierTest.scala
+++ src/test/scala/NaiveBayesClassifierTest.scala
@@ -4,21 +4,25 @@ import reader.{CSVReader, CsvDocument}
 class NaiveBayesClassifierTest extends Specification {
 
   "NaiveBayesClassifier " should {
+    val toDocumentTuple: CsvDocument => (Document, DocClass) = csv => (new Document(csv.getText), new DocClass(csv.getCategory.toString))
+    val negatives = CSVReader.read("negative.csv").map(toDocumentTuple)
+    val positives = CSVReader.read("positive.csv").map(toDocumentTuple)
+
+    val alg = new NaiveBayesLearningAlgorithm(negatives ++ positives)
+
+    val tuple = negatives.apply(40)
+    val expectedClass: String = tuple._2.get
+    assert(expectedClass == (-1).toString)
+
+    val docClassification: DocClassification = alg.classifier.classify(tuple._1.text)
+    val actualClass: String = docClassification.docClass.get
+
     "be able to classify negative text" in {
-      val toDocumentTuple: CsvDocument => (Document, DocClass) = csv => (new Document(csv.getText), new DocClass(csv.getCategory.toString))
-      val negatives = CSVReader.read("negative.csv").map(toDocumentTuple)
-      val positives = CSVReader.read("positive.csv").map(toDocumentTuple)
+      actualClass.shouldEqual(expectedClass)
+    }
 
-      val alg = new NaiveBayesLearningAlgorithm(negatives ++ positives)
-
-      val tuple = negatives.apply(40)
-      val expected: String = tuple._2.get
-      assert(expected == (-1).toString)
-
-      val bestClass: DocClassification = alg.classifier.classify(tuple._1.text)
-      val actual: String = bestClass.docClass.get
-
-      actual.shouldEqual(expected)
+    "return highlighted text" in {
+      docClassification.highlightedText.get shouldEqual "*highlightedText*"
     }
   }
 }
